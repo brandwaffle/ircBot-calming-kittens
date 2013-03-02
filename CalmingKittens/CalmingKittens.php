4619f53d4e62bb88b7c7ef6c694a5ecaef6728b1
Vasken Hauri <vhauri@gmail.com>
2013-03-01 19:01:32 -0800

    Massive improvements to language. We're talking about going from NSYNC to Shakespeare here, folks

--- CalmingKittens/CalmingKittens.php
+++ CalmingKittens/CalmingKittens.php
@@ -13,7 +13,7 @@ class CalmingKittens{
 
     public function check_commands( $username = '', $channel = '', $message = '' ){
         if( preg_match( '/^!calmdown/i', $message ) ){
-            ircBot::send_channel_message( $channel, $username . 'You seem stressed. Maybe you should look at kittens http://farm5.staticflickr.com/4060/4647807839_8206b541fb_z.jpg' );
+            ircBot::send_channel_message( $channel, 'You seem stressed, ' . $username . '. Maybe you should look at kittens http://farm5.staticflickr.com/4060/4647807839_8206b541fb_z.jpg' );
         }
     }
 }
